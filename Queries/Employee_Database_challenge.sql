f65e9e556f4eb3b3eb21621018e31a564012fcd2
Pierre-Olivier Boisvert <pierre-olivierboisvert@Pierre-Oliviers-MacBook-Pro.local>
2021-02-14 16:38:51 -0500

    Revised picture

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,97 @@
+-- Deliverable 1: The Number of Retiring Employees of each title category START (0 - 7)
+SELECT 
+    em.emp_no,
+    em.first_name,
+    em.last_name,
+    ti.title,
+    ti.from_date,
+    ti.to_date
+INTO retirement_titles
+FROM employees as em
+INNER JOIN titles AS ti
+ON (em.emp_no = ti.emp_no)
+WHERE em.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY em.emp_no ASC;
+-- Querie test below
+--
+-- SELECT * FROM retirement_titles ;
+--
+-- Deliverable 1: The Number of Retiring Employees of each title category END
+-- Employee soon retired Aggregation and duplicated removed START (8 - 15)
+SELECT 
+    DISTINCT ON (rt.emp_no)
+	rt.emp_no,
+    rt.first_name,
+    rt.last_name,
+    rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no ASC, rt.to_date DESC;
+-- Employee soon retired Aggregation and duplicated removed START (8 - 15) END
+-- COUNT employee by title soon retired (15 - 21) START
+SELECT 
+	COUNT(emp_no),
+	title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC
+-- COUNT employee by title soon retired (15 - 21) END
+-- Challenge PART 2
+-- The Employees Eligible for the Mentorship Program START
+SELECT 
+	DISTINCT ON (em.emp_no)
+    em.emp_no,
+    em.first_name,
+    em.last_name,
+    em.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees as em
+INNER JOIN dept_emp AS de
+ON (em.emp_no = de.emp_no)
+INNER JOIN (SELECT DISTINCT emp_no, title FROM titles) AS ti
+ON (em.emp_no = ti.emp_no)
+WHERE em.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+AND (de.to_date = '9999-01-01')
+ORDER BY em.emp_no ASC
+-- Querie test below
+--
+-- SELECT * FROM titles
+-- 
+--The Employees Eligible for the Mentorship Program END
+-- Generate a list of employee by title that started on or after 1999-12-31
+SELECT 
+	COUNT(me.emp_no),
+	SUM(sa.salary)/COUNT(me.emp_no) AS "Average Salary",
+	d.dept_name
+FROM mentorship_eligibilty as me
+INNER JOIN salaries AS sa
+ON (me.emp_no = sa.emp_no) 
+INNER JOIN dept_emp AS de
+ON (me.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dep_no = d.dept_no)
+WHERE me.from_date >= '19991231'
+GROUP BY d.dept_no
+ORDER BY COUNT(me.emp_no) DESC
+
+-- Generate a group by title and the average salary
+SELECT 
+	COUNT(me.emp_no),
+	SUM(sa.salary)/COUNT(me.emp_no) AS "Average Salary",
+	d.dept_name
+FROM mentorship_eligibilty as me
+INNER JOIN salaries AS sa
+ON (me.emp_no = sa.emp_no) 
+INNER JOIN dept_emp AS de
+ON (me.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dep_no = d.dept_no)
+GROUP BY d.dept_no
+ORDER BY COUNT(me.emp_no) DESC
+
+SELECT * FROM departments
+
--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# Pewlett-Hackard-Analysis
+## Project Overview
+
+The purpose of this project is to investigate and find recommendation in a list of 300,024 number of employees working at Pewlett Hackard. We want to (1) identify the ones that are eligible for retirement, (2) and the ones that are in the list of eligibility of mentorship. We complete the analysis, we imported and created a PostgreSQL database with queries in order to generate report that have been exported to CSV files.
+
+## Resources
+
+Datasets (Data folder): departments.csv, dept_emp.csv, dept_manager.csv, employees.csv, salaries.csv, titles.csv, retirement_info.csv
+Software: PostgreSQL 11.10 and QuickDBD (https://www.quickdatabasediagrams.com/)
+
+## Results
+
+We the help of SQL in pgAdmin4, querries are written and the results are as listed below:
+
+- mentorship_eligibility
+- retirement_titles
+- retiring_titles
+- unique_titles
+
+### Step 1: Database Schema & importation
+
+Figure 1: PostgreSQL schema
+<img src="https://github.com/poboisvert/Pewlett-Hackard-Analysis/blob/main/EmployeeDB.png?raw=true" width="750" />
+
+### Step 2: additional tables
+
+Following tables are created in order to have additional insights.
+
+- retirement_titles (generate a list of retiring employees)
+- unique_titles (Select the lasted job title for the employees in retirement_titles
+- retiring_titles (Generate a count by job title from the list unique_titles)
+
+## Summary
+
+In this project to acheive the goal, we needed to create some other beneficial tables as well, which could be usefull anytime for the manager requests and help them current system analysis and decision making.
+
+### Table 1: mentorship_eligibilty
+
+This table give a list of 1,550 employees that fit into the "silver tsunami" and will reach soon the retirement age.
+
+## Two additional querries
+
+To complete and help to identify current employees, and gives informations about retirement based on current employees.
+
+```
+SELECT
+	COUNT(me.emp_no),
+	SUM(sa.salary)/COUNT(me.emp_no) AS "Average Salary",
+	d.dept_name
+FROM mentorship_eligibilty as me
+INNER JOIN salaries AS sa
+ON (me.emp_no = sa.emp_no)
+INNER JOIN dept_emp AS de
+ON (me.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dep_no = d.dept_no)
+GROUP BY d.dept_no
+ORDER BY COUNT(me.emp_no) DESC
+```
+
+```
+SELECT
+    me.emp_no,
+    me.first_name,
+    me.last_name,
+    me.birth_date,
+	me.from_date,
+	me.title,
+	sa.salary
+FROM mentorship_eligibilty as me
+INNER JOIN salaries AS sa
+ON (me.emp_no = sa.emp_no)
+WHERE me.from_date >= '19991231'
+ORDER BY me.from_date DESC
+```
